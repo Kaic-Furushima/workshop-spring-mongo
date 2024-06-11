992dc81c099158ef99850c3e66e80e3ac34e7050
Kaic-Furushima <kaic.fbs@gmail.com>
2024-06-11 16:50:53 -0300

    Project Creation

---
+++ src/test/java/com/kaicf/workshopspringmongo/WorkshopSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaicf.workshopspringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
