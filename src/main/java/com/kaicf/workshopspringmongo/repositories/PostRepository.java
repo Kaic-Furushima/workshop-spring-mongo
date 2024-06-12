acec5441548e2a01fad5ec78de39f3362caebebb
Kaic-Furushima <kaic.fbs@gmail.com>
2024-06-12 01:12:39 -0300

    Entity Post

---
+++ src/main/java/com/kaicf/workshopspringmongo/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.kaicf.workshopspringmongo.repositories;
+
+import com.kaicf.workshopspringmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
