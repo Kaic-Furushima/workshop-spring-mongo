55709c852d010ee1aee11574bf2aac6bc23ab808
Kaic-Furushima <kaic.fbs@gmail.com>
2024-06-12 01:48:07 -0300

    findById method for posts

---
+++ src/main/java/com/kaicf/workshopspringmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.kaicf.workshopspringmongo.services;
+
+import com.kaicf.workshopspringmongo.domain.Post;
+import com.kaicf.workshopspringmongo.repositories.PostRepository;
+import com.kaicf.workshopspringmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+}
